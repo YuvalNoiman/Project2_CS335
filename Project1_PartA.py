acd57d63d04fd11e08fde063c862cb9ce68c8455
YuvalNoiman <89434412+YuvalNoiman@users.noreply.github.com>
2023-10-09 13:00:24 -0700

    Create Project1_PartA.py

---
+++ Project1_PartA.py
@@ -0,0 +1,24 @@
+# Name: Yuval Noiman
+# Date: 10/9/2023
+# File Purpose: Calculates stock purchase maximization using exhaustive search approach
+import ast
+
+def main():
+
+   file1 = open("Input.txt", "r")
+   file2 = open("Output.txt","w")
+   while True:
+      try:
+         size_of_array  = int(file1.readline(()
+         stocks_and_values = ast.literal_eval(file1.readline())
+         Amount = int(file1.readline())
+         file1.readline()
+         #file2.write(str()+"\n\n")
+      except:
+         break
+
+   file1.close()
+   file2.close()
+
+if __name__ == "__main__":
+    main()

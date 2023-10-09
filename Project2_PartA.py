cf28b1d737b6c3545062679718345db3437b2526
YuvalNoiman <89434412+YuvalNoiman@users.noreply.github.com>
2023-10-09 13:33:59 -0700

    Update Project2_PartA.py

--- Project2_PartA.py
+++ Project2_PartA.py
@@ -3,15 +3,29 @@
 # File Purpose: Calculates stock purchase maximization using exhaustive search approach
 import ast
 
+def create_combinations(size, items):
+
 def verify_combinations(M, items, canidate):
-   return True
+   if (len(canidate) > len(items)):
+      return False
+   amount = 0
+   for x in range(len(canidate)):
+      amount = amount + canidate[x][1]
+   if (amount <= M):
+      return True
+   return False
 
 def total_value(canidate):
-   return 0
+   if (canidate == None):
+      return 0
+   total = 0
+   for x in range(len(canidate)):
+      total = total + canidate[x][0]
+   return total
    
 def stock_maximization(M, items):
    best = None
-   for canidate in range :
+   for canidate in range(len(items)):
       if verify_combinations(M, items, canidate):
          if ((best == None) or (total_value(canidate) > total_value(best))):
             best = canidate

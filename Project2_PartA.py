8cd6f7e57f0e6a84bf505aa9371d9c675ec0214d
YuvalNoiman <89434412+YuvalNoiman@users.noreply.github.com>
2023-11-06 16:04:43 -0800

    Update Project2_PartA.py

--- Project2_PartA.py
+++ Project2_PartA.py
@@ -3,47 +3,13 @@
 # File Purpose: Calculates stock purchase maximization using exhaustive search approach
 import ast
 
-def create_combinations(size, items):
-   combinations = []
-   i = 1
-   while (i <= size):  
-      combinations.append(items)
-      i = i + 1
-   #for x in range(1, (size * size)):
-    #  combinations.append(
-   return combinations
-
 def create_combinations(size, items, combinations):
-   if (size == 0): #size = 0
+   if (size == 0):
        return
    if (items not in combinations):
       for x in range(0, size):
-      #combinations.append(items[x:size])
-     #print(items[0:x])
-     #print(items[x+1:size-1])
          create_combinations(size-1, items[0:x]+items[x+1:size], combinations)
       combinations.append(items)
-   #create_combinations(size-1, items[0:size-2], combinations)
-   """if ((size%2) == 0):
-      y = int(size/2)
-      print(y)
-      list = items[0:y]+items[y+1:size]
-      return create_combinations(size-1, list, combinations)
-   if ((size%2) == 1):
-      y = int(size/2)+1
-      print(y)
-      list = items[0:y]+items[y+1:size]
-      return create_combinations(size-1, list, combinations)"""
-   #return create_combinations(size-1, items[1:size], combinations)
-   
-def powerset(seq): #returns wrong number
-   if len(seq) == 1:
-      yield seq
-      #yield []
-   else:
-      for item in powerset(seq[1:]):
-         yield [seq[0]]+item
-         yield item
 
 def verify_combinations(M, items, canidate):
    value = items[canidate]
@@ -78,34 +44,19 @@ def main():
    file2 = open("OutputA.txt","w")
    while True:
       try:
-         #print(file1.readline())
          size_of_array  = int(file1.readline())
          stocks_and_values = ast.literal_eval(file1.readline())
          amount = int(file1.readline())
          file1.readline()
          combo = []
          create_combinations(size_of_array, stocks_and_values, combo)
-         print(combo)
-         #combinations = [x for x in powerset(stocks_and_values)]
-         #print(combinations)
-         #print(stock_maximization(amount,combinations))
+         #print(combo)
          print(stock_maximization(amount, combo))
-         #file2.write(str(stock_maximization(amount,combinations))+"\n\n")
          file2.write(str(stock_maximization(amount,combo))+"\n\n")
       except:
          break
-
    file1.close()
    file2.close()
 
 if __name__ == "__main__":
-    #combo = []
-    #create_combinations(7, [1, 2, 3, 4, 5, 6, 7], combo)
-    #print(combo)
-    #print(len(combo))
-    #res = []
-    #for i in combo:
-      #  if i not in res:
-    #        res.append(i)
-   # print(len(res))
     main()
